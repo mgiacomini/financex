256e2601589d10e95447f63fcb9f62943333e569
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2018-04-13 14:29:54 -0300

    First commit

---
+++ lib/financex.ex
@@ -0,0 +1,18 @@
+defmodule Financex do
+  @moduledoc """
+  Documentation for Financex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Financex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/financex_test.exs
@@ -0,0 +1,8 @@
+defmodule FinancexTest do
+  use ExUnit.Case
+  doctest Financex
+
+  test "greets the world" do
+    assert Financex.hello() == :world
+  end
+end
